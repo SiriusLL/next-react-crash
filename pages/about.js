0788ad7ee8cf9911fda8807e46cd880bf3e14eaa
SiriusLL <sirius.love.light@gmail.com>
2022-01-18 15:49:09 -0700

    custom meta component

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import Meta from "../components/Meta";
 
 const about = () => {
   return (
     <div>
+      <Meta title="About" />
       <h1>about</h1>
     </div>
-  )
-}
+  );
+};
 
-export default about
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import { server } from "../config";
-import Head from "next/head";
+
 import ArticleList from "../components/ArticleList";
 
 export default function Home({ articles }) {
   // console.log(articles);
   return (
     <div>
-      <Head>
-        <title>WebDev Newz</title>
-        <meta name="heywords" content="web development, programming" />
-      </Head>
       <ArticleList articles={articles} />
     </div>
   );
