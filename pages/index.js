3aca8dfeb197cfd42ea30dd60fb948244cb669bb
SiriusLL <sirius.love.light@gmail.com>
2022-01-18 14:25:17 -0700

    getServerSideProps, Nested Routing, Show Data

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
-import Head from 'next/head'
+import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
-export default function Home() {
+export default function Home({ articles }) {
+  // console.log(articles);
   return (
-    <div >
+    <div>
       <Head>
         <title>WebDev Newz</title>
-        <meta name='heywords' content='web development, programming' />
+        <meta name="heywords" content="web development, programming" />
       </Head>
-      <h2>Welcome to Next</h2>
+      <ArticleList articles={articles} />
     </div>
-  )
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
